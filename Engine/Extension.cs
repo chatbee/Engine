408b77f18b6a5d627de4b17d4a379a8004923616
atotalnoob <atotalnoob@users.noreply.github.com>
2020-01-18 16:05:36 -0800

    added extension method for DI

---
+++ Engine/Extension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Chatbees.Engine
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddChatbeesEngine(this IServiceCollection services, MemoryCacheOptions cacheOptions)
+        {
+            services.AddSingleton<IEngineService>(new EngineService(cacheOptions));
+            return services;
+        }
+    }
+}
